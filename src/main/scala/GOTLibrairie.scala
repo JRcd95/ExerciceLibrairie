986c29bf542ad5df094ba5ec210a3d4620e3e79c
JRcd95 <joanieruchaud@gmail.com>
2020-09-19 23:59:56 +0200

    3eme etape : calcule du prix d'un pack (1,2,3,4,5 tomes differents)

--- src/main/scala/GOTLibrairie.scala
+++ src/main/scala/GOTLibrairie.scala
@@ -1,40 +1,28 @@
 object GOTLibrairie {
 
-  def calculePrixPanier(nb1: Int): Double = {
-    if (nb1 == 0) {
-      val prixPanier = 0.0
-      prixPanier
-    } else if (nb1 == 1) {
-      val prixPanier = 8.0
-      prixPanier
-    } else if (nb1 == 2) {
-      val prixPanier = 15.20
-      prixPanier
-    } else if (nb1 == 3) {
-      val prixPanier = 21.60
-      prixPanier
-    }else if (nb1 == 4) {
-      val prixPanier = 25.60
-      prixPanier
+  def calculePrixPack(nbTomeDifferent: Int): Double = {
+    if (nbTomeDifferent == 0) {
+       0.0
+    } else if (nbTomeDifferent == 1) {
+       8.0
+    } else if (nbTomeDifferent == 2) {
+      15.20
+    } else if (nbTomeDifferent == 3) {
+      21.60
+    }else if (nbTomeDifferent == 4) {
+      25.60
     }else {
-      val prixPanier = 30.0
-      prixPanier
+      30.0
     }
-
   }
 
-  def calculePanier(nbTomeI: Int, nbTomeII: Int, nbTomeIII: Int, nbTomeIV: Int, nbTomeV: Int): Unit = {
+  def calculePrixPanier(nbTomeI: Int, nbTomeII: Int, nbTomeIII: Int, nbTomeIV: Int, nbTomeV: Int): Double = {
     if (nbTomeI <= 1 && nbTomeII <=1 && nbTomeIII <=1 && nbTomeIV <=1 && nbTomeV <=1) {
       val nbTotalTome = nbTomeI + nbTomeII + nbTomeIII + nbTomeIV + nbTomeV
-      println(nbTotalTome)
+      calculePrixPack(nbTotalTome)
     } else {
-      println("error")
+      -1.0
     }
-
   }
-  
-
-
-
 
 }
