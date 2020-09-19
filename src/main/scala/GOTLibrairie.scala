70a488a96d75ba2faeb783f0d935cae15e2e4089
JRcd95 <joanieruchaud@gmail.com>
2020-09-19 19:11:11 +0200

    2eme etape : calcule prix total avec remise

--- src/main/scala/GOTLibrairie.scala
+++ src/main/scala/GOTLibrairie.scala
@@ -2,13 +2,32 @@ object GOTLibrairie {
 
   def calculePanier(nbTomeI: Int, nbTomeII: Int, nbTomeIII: Int, nbTomeIV: Int, nbTomeV: Int): Int = {
     val nbTotalTome = nbTomeI + nbTomeII + nbTomeIII + nbTomeIV + nbTomeV
-    val prixTotal = nbTotalTome*8
+    val prixTotal = nbTotalTome * 8
     prixTotal
   }
-  def calculePack(): Int ={
-    val nbTome = List(1,1,2,0,0)
-    val nbPack = nbTome.max
-    nbPack
+
+  def calculePrixPanier(nb1: Int): Double = {
+    if (nb1 == 0) {
+      val prixPanier = 0.0
+      prixPanier
+    } else if (nb1 == 1) {
+      val prixPanier = 8.0
+      prixPanier
+    } else if (nb1 == 2) {
+      val prixPanier = 15.20
+      prixPanier
+    } else if (nb1 == 3) {
+      val prixPanier = 21.60
+      prixPanier
+    }else if (nb1 == 4) {
+      val prixPanier = 25.60
+      prixPanier
+    }else {
+      val prixPanier = 30.0
+      prixPanier
+    }
+
   }
+
 
 }
