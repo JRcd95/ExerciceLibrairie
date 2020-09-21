abc458ebaac4fbf5f7fb92db87bfff8d98ba61ba
JRcd95 <joanieruchaud@gmail.com>
2020-09-21 17:51:15 +0200

    4eme etape : correction des tests + calcule du prix de deux packs

--- src/main/scala/GOTLibrairie.scala
+++ src/main/scala/GOTLibrairie.scala
@@ -2,32 +2,63 @@ object GOTLibrairie {
 
   def calculePrixPack(nbTomeDifferent: Int): Double = {
     if (nbTomeDifferent == 0) {
-       0.0
+      0.0
     } else if (nbTomeDifferent == 1) {
-       8.0
+      8.0
     } else if (nbTomeDifferent == 2) {
       15.20
     } else if (nbTomeDifferent == 3) {
       21.60
-    }else if (nbTomeDifferent == 4) {
+    } else if (nbTomeDifferent == 4) {
       25.60
-    }else {
+    } else {
       30.0
     }
   }
 
   def calculePrixPanier(tomes: List[Int]): Double = {
-    if (tomes(0) <= 1 && tomes(1) <= 1 && tomes(2) <= 1 && tomes(3) <= 1 && tomes(4) <= 1)  {
-      val nbTotalTome = tomes(0) + tomes (1) + tomes (2) + tomes (3) + tomes (4)
-      calculePrixPack(nbTotalTome)
-    } else {
-      -1.0
+    if (tomes.max >= 2) {
+      val tomeRestants: List[Int] = {
+        for {
+          i <- tomes
+        } yield {
+          if (i != 0) i - 1
+          else i
+        }
+      }
+      val pack = tomes.sum - tomeRestants.sum
+      val pack2 = tomeRestants.sum
+      val total = calculePrixPack(pack) + calculePrixPack(pack2)
+      total
+    }else {
+      calculePrixPack(tomes.sum)
     }
   }
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+      /*tomes.dropWhile((n :Int) => {n==0})
+
+      for (i <- tomes) yield i-1
+      println(tomes)
+
+      (tomes(0) != 0 && tomes(1) != 0 && tomes(2) != 0 && tomes(3) != 0 && tomes(4) != 0)
+
+
+      -1.0*/
+
+
+
+
+
+
+
+
+
+
+
--- src/test/scala/GOTLibrairieTest.scala
+++ src/test/scala/GOTLibrairieTest.scala
@@ -4,31 +4,31 @@ import org.scalatest.Matchers.{be, convertToAnyShouldWrapper}
 class GOTLibrairieTest extends FunSuite{
 
   test("Panier vide") {
-    GOTLibrairie.calculePrixPanier(0,0,0,0,0) should be (0.0)
+    GOTLibrairie.calculePrixPanier(List(0,0,0,0,0)) should be (0.0)
   }
   test("Panier avec un seul tome") {
-    GOTLibrairie.calculePrixPanier(1,0,0,0,0) should be (8.0)
+    GOTLibrairie.calculePrixPanier(List(1,0,0,0,0)) should be (8.0)
   }
   test("Panier avec deux tomes différents") {
-    GOTLibrairie.calculePrixPanier(1,1,0,0,0) should be (15.2)
+    GOTLibrairie.calculePrixPanier(List(1,1,0,0,0)) should be (15.2)
   }
   test("Panier avec trois tomes différents") {
-    GOTLibrairie.calculePrixPanier(1,1,1,0,0) should be (21.6)
+    GOTLibrairie.calculePrixPanier(List(1,1,1,0,0)) should be (21.6)
   }
   test("Panier avec quatre tomes différents") {
-    GOTLibrairie.calculePrixPanier(1,1,1,1,0) should be (25.6)
+    GOTLibrairie.calculePrixPanier(List(1,1,1,1,0)) should be (25.6)
   }
   test("Panier avec cinq tomes différents") {
-    GOTLibrairie.calculePrixPanier(1,1,1,1,1) should be (30.0)
+    GOTLibrairie.calculePrixPanier(List(1,1,1,1,1)) should be (30.0)
   }
   test("Panier deux packs de un tome") {
-    GOTLibrairie.calculePrixPanier(2,0,0,0,0) should be (16.0)
+    GOTLibrairie.calculePrixPanier(List(2,0,0,0,0)) should be (16.0)
   }
   test("Panier deux packs de deux tomes") {
-    GOTLibrairie.calculePrixPanier(2,2,0,0,0) should be (30.2)
+    GOTLibrairie.calculePrixPanier(List(2,2,0,0,0)) should be (30.4)
   }
   test("Panier deux packs de quatres tomes") {
-    GOTLibrairie.calculePrixPanier(2,2,2,1,1) should be (51.2)
+    GOTLibrairie.calculePrixPanier(List(2,2,2,1,1)) should be (51.2)
   }
 
 }
