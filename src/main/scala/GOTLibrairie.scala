5705a2bf6ac7fa616b4933ce12e40ce4bf77f5fe
JRcd95 <joanieruchaud@gmail.com>
2020-10-05 08:38:18 +0200

    Simplification du code

--- src/main/scala/GOTLibrairie.scala
+++ src/main/scala/GOTLibrairie.scala
@@ -7,11 +7,11 @@ object GOTLibrairie {
     } else if (nbTomeDifferent == 1) {
       8.0
     } else if (nbTomeDifferent == 2) {
-      15.20
+      15.2
     } else if (nbTomeDifferent == 3) {
-      21.60
+      21.6
     } else if (nbTomeDifferent == 4) {
-      25.60
+      25.6
     } else {
       30.0
     }
@@ -21,9 +21,8 @@ object GOTLibrairie {
     if (tomes.max >= 2) {
       val tomeRestants: List[Int] = tomes.map(i=> if (i!=0) i-1 else i)
       val prixPanier = calculePrixPack(tomes.sum - tomeRestants.sum) + calculePrixPanier(tomeRestants)
-      if (prixPanier == 51.6 ){
-        val remiseMax : Double = 51.2
-        remiseMax
+      if (prixPanier == 51.6 ){ // prix d'un panier avec un pack de 3 et un de 5
+         51.2 // prix d'un panier avec deux packs de 4, maximisation de la remise
       }else
         prixPanier
     }else {
